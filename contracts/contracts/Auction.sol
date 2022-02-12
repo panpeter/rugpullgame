33b0718a033b581d055e29d65a912992b5b8a08e
Piotr Wilczek <piotrwilczek@gmail.com>
2022-02-12 13:16:12 +0300

    Implement basic Auction contract

--- contracts/contracts/Auction.sol
+++ contracts/contracts/Auction.sol
@@ -2,8 +2,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract Auction is Ownable {
-    
+contract Auction {
+    uint256 public constant BID_PRICE = 1 ether;
+    uint256 public constant CLAIM_BLOCKS = 10;
+
+    uint256 private lastBidBlock = 0;
+    address payable[] private lastBidders;
+
+    function bid() external payable {
+        require(msg.value >= BID_PRICE, "Bid price must be 1 ether or more");
+
+        if (lastBidBlock != block.number) {
+            delete lastBidders;
+            lastBidBlock = block.number;
+        }
+
+        lastBidders.push(payable(msg.sender));
+    }
+
+    function getLastBidders() public view returns(address payable[] memory) {
+        return lastBidders;
+    }
+
+    function claim() external {
+        require(
+            block.number >= lastBidBlock + CLAIM_BLOCKS,
+            "At least 10 blocks must pass before a claim is made"
+        );
+
+        uint256 reward = address(this).balance / lastBidders.length;
+        for (uint256 i = 0; i < lastBidders.length; i++) {
+            lastBidders[i].transfer(reward);
+        }
+        delete lastBidders;
+    }
 }
