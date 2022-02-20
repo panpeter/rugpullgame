5d1f91b97e45575213b851c1965a69ec5f6d26ec
Piotr Wilczek <piotrwilczek@gmail.com>
2022-02-20 14:03:16 +0300

    Add polygon_mainnet network

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,15 +3,24 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+const {
+   POLYGON_MUMBAI_API_URL,
+   POLYGON_MUMBAI_PRIVATE_KEY,
+   POLYGON_MAINNET_API_URL,
+   POLYGON_MAINNET_PRIVATE_KEY
+} = process.env;
 module.exports = {
    solidity: "0.8.0",
    defaultNetwork: "hardhat",
    networks: {
       hardhat: {},
       polygon_mumbai: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         url: POLYGON_MUMBAI_API_URL,
+         accounts: [`0x${POLYGON_MUMBAI_PRIVATE_KEY}`]
+      },
+      polygon_mainnet: {
+         url: POLYGON_MAINNET_API_URL,
+         accounts: [`0x${POLYGON_MAINNET_PRIVATE_KEY}`]
       }
    },
    settings: {
